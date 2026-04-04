89c5845a9c4077f44bba35f9c1df095824f193a7
AmiruHoradagoda <amirumithsara1234@gmail.com>
2026-04-04 21:15:09 +0530

    first commit

---
+++ src/main/java/com/ec7205/event_hub/event_service_api/EventServiceApiApplication.java
@@ -0,0 +1,13 @@
+package com.ec7205.event_hub.event_service_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventServiceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventServiceApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ec7205/event_hub/event_service_api/EventServiceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ec7205.event_hub.event_service_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventServiceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
