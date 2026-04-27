3140084ba13e08530471bfd97b5aabed2c6ae417
AmiruHoradagoda <amirumithsara1234@gmail.com>
2026-04-27 22:54:27 +0530

    feat: Update CI/CD workflow and add test configurations; include H2 database dependency

--- src/test/java/com/ec7205/event_hub/event_service_api/EventServiceApiApplicationTests.java
+++ src/test/java/com/ec7205/event_hub/event_service_api/EventServiceApiApplicationTests.java
@@ -2,8 +2,10 @@ package com.ec7205.event_hub.event_service_api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class EventServiceApiApplicationTests {
 
 	@Test
